a4923af2e5f2e51e20283f5bfc974bae982ab40b
brianma03 <bmaguilar03@gmail.com>
2023-06-16 10:30:04 -0400

    Added a router link attribute to heroes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'detail/:id', component: HeroesComponent },
 ];
 
 @NgModule({
